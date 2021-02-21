b250e7567621cdc03e0b02d4b16ceeec464a10df
psu-powerlab <44616832+psu-powerlab@users.noreply.github.com>
2021-02-21 13:36:19 -0800

    ok the virtual detor for the adapter interface was causing issues

--- xml/include/xml/adapter_interface.hpp
+++ xml/include/xml/adapter_interface.hpp
@@ -7,10 +7,7 @@ namespace xml
     class AdapterInterface
     {
     public:
-        virtual ~AdapterInterface ();
         virtual std::string serialize() = 0; // pure virtual
-
-    private:
 
     };
 } // namespace xml
