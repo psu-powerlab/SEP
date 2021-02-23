1a5ab8423b70fdf5df200e004881719b60d8e6de
psu-powerlab <44616832+psu-powerlab@users.noreply.github.com>
2021-02-23 13:54:12 -0800

    It wasn't the boost read_xml streaming, but the streaming of the enum class that was the issue. I wrote in a quick as_value function, but need to find a good place for it somewhere.

--- test/xml/main.cpp
+++ test/xml/main.cpp
@@ -1,11 +1,5 @@
 #include "gtest/gtest.h"
 
-template <typename Enumeration>
-auto as_value(Enumeration const value)
-    -> typename std::underlying_type<Enumeration>::type
-{
-    return static_cast<typename std::underlying_type<Enumeration>::type>(value);
-};
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
--- xml/active_power_adapter.cpp
+++ xml/active_power_adapter.cpp
@@ -1,12 +1,20 @@
 #include <sstream>
+#include <type_traits>
 #include <boost/property_tree/xml_parser.hpp>
 #include "include/xml/active_power_adapter.hpp"
+
+template <typename Enumeration>
+auto as_value(Enumeration const value)
+    -> typename std::underlying_type<Enumeration>::type
+{
+    return static_cast<typename std::underlying_type<Enumeration>::type>(value);
+};
 
 namespace xml
 {
     ActivePowerAdapter::ActivePowerAdapter(const sep::ActivePower &active_power)
     {
-        tree_.put("ActivePower.multiplier", active_power.multiplier_);
+        tree_.put("ActivePower.multiplier", as_value(active_power.multiplier_));
         tree_.put("ActivePower.value", active_power.value_);
     }
     
